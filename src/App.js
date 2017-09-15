f2647672ee64753710b43c45f63d66601255f09d
hiterharris <you@example.com>
2017-09-15 18:45:02 -0400

    ui coded, guest state added

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import './App.css';
+import GuestList from './GuestList.js';
+
+class App extends Component {
+
+  state = {
+    guests: [
+      {
+        name: 'Liza',
+        isConfirmed: false,
+      },
+      {
+        name: 'Michael',
+        isConfirmed: false,
+      },
+      {
+        name: 'Samantha',
+        isConfirmed: false,
+      }
+    ]
+  };
+
+  toggleConfirmationAt = indexToChange => 
+    this.setState({
+      guests: this.state.guests.map((guest, index) => {
+        if (index === indexToChange) {
+          return {
+            ...guest,
+            isConfirmed: !guest.isConfirmed
+          };
+        }
+          return guest;
+      })
+    });
+
+  getTotalInvited = () => this.state.guests.length
+  // getAttendingGuests = () =>
+  // getUnconfirmedGuests = () =>
+
+  render() {
+    return (
+      <div className="App">
+      <header>
+        <p>Manage your guest list with</p>
+        <h1>RSVP</h1>
+        <form>
+            <input type="text" value="Safia" placeholder="Invite Someone" />
+            <button type="submit" name="submit" value="submit">Submit</button>
+        </form>
+      </header>
+      <div className="main">
+        <div>
+          <h2>Invitees</h2>
+          <label>
+            <input type="checkbox" /> Hide those who haven't responded
+          </label>
+        </div>
+        <table className="counter">
+          <tbody>
+            <tr>
+              <td>Attending:</td>
+              <td>2</td>
+            </tr>
+            <tr>
+              <td>Unconfirmed:</td>
+              <td>1</td>
+            </tr>
+            <tr>
+              <td>Total:</td>
+              <td>3</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <GuestList guests={this.state.guests}/>
+
+      </div>
+    </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/GuestList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Guest from './Guest.js';
+
+const GuestList = props =>
+    <ul>
+        {props.guests.map((guest, index) => 
+            <Guest key={index} name={guest.name} isConfirmed={guest.isConfirmed}/>
+        )}
+    </ul>
+
+GuestList.propType = {
+    guests: PropTypes.array.isRequired
+}
+
+export default GuestList;
